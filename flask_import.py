71c639a096068ccd954aabb34792d7962fb37341
sherri lee <75329306+slpmssg12@users.noreply.github.com>
2021-10-29 15:50:49 -0400

    Update flask_import.py

--- flask_import.py
+++ flask_import.py
@@ -5,21 +5,18 @@ app = Flask(__name__) # "__main__"
 @app.route('/grp7', methods=['GET'])
 def flask_import():
   return """<html>
-  
-<head>
-<title>FA595 Midterm</title>
-</head>
+ 
 <body>
-<h1>Group 7: Joseph Chionchio, Ashley-Marie Fontes, Sherri Putnam, Gloria Rumao</h1>
+<h1>FA595 Midterm<h1><br><br>
+<h2>Group 7: Joseph Chionchio, Ashley-Marie Fontes, Sherri Putnam, Gloria Rumao</h2>
 <b>
-<p>Deliverables<br>
+<p>Deliverables:<br>
 
 1. Created a flask end point accessible to the entire internet hosted on an AWS machine<br>
 
 2. Able to accept cURL calls on one route and successfully return a 200 status when called<br>
 
-3. Able to accept in a request with a payload including a string and a requested NLP service<br> 
-   Our project is of returning 2 services for each member of our group<br>
+3. Able to accept in a request with a payload including a string and a requested NLP service. Our project is of returning 2 services for each member of our group<br>
 </p>
 </body>
   </html>
