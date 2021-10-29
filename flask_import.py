348bd7aaba3a56debb6068f3d960dda5ed37f163
sherri lee <75329306+slpmssg12@users.noreply.github.com>
2021-10-29 15:48:12 -0400

    Update flask_import.py

--- flask_import.py
+++ flask_import.py
@@ -12,14 +12,14 @@ def flask_import():
 <body>
 <h1>Group 7: Joseph Chionchio, Ashley-Marie Fontes, Sherri Putnam, Gloria Rumao</h1>
 <b>
-<p>Deliverables<b><b>
+<p>Deliverables<br>
 
-1. Created a flask end point accessible to the entire internet hosted on an AWS machine<b>
+1. Created a flask end point accessible to the entire internet hosted on an AWS machine<br>
 
-2. Able to accept cURL calls on one route and successfully return a 200 status when called<b>
+2. Able to accept cURL calls on one route and successfully return a 200 status when called<br>
 
-3. Able to accept in a request with a payload including a string and a requested NLP service<b> 
-   Our project is of returning 2 services for each member of our group<b>
+3. Able to accept in a request with a payload including a string and a requested NLP service<br> 
+   Our project is of returning 2 services for each member of our group<br>
 </p>
 </body>
   </html>
